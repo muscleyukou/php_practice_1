52ee6b61e217675dd563418f08266903fb2e9f3e
muscleyukou <yukou29good0910@gmail.com>
2020-06-20 15:14:22 +0900

    utfの変更

--- form4/utill.php
+++ form4/utill.php
@@ -3,7 +3,7 @@ function es($data,$charset){
     if(is_array($data)){
         return array_map(__METHOD__,$data);
     }else{
-        return htmlspecialchars($data,ENT_QUOTES,$charset);
+        return htmlspecialchars($data,ENT_QUOTES,'UTF-8');
     }
 }
 ?>
